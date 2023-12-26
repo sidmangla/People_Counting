1c04eabc00d0f3a6f5c7cb8d130e6bab53f32690
sidmangla <34414176+sidmangla@users.noreply.github.com>
2023-12-26 03:20:13 +0000

    Add files via upload

--- security_check.py
+++ security_check.py
@@ -7,7 +7,6 @@ import numpy as np
 
 class SecurityCheck():
 	def __init__(self):
-		self.FLAG = False
 		self.person_dict = {}
 		self.del_list=[]
 		self.not_checked = []
@@ -36,6 +35,40 @@ class SecurityCheck():
 		self.sec_trig_ylimit = 120
 		self.checked_count = 0
 		self.security_check = 0
+		self.x_res = 1280
+		self.y_res = 720
+
+	def load_file(self,file):
+		with open(file) as f:
+			info = json.load(f)
+			#model parameters
+			self.weightPath = info['weightPath']
+			self.metaPath = info['metaPath']
+			self.configPath = info['configPath']
+
+			self.entry_point = int(info["entry_point"])
+			self.exit_point = int(info["exit_point"])
+			self.first_phase = info["first_phase"]
+			self.second_phase = info["second_phase"]
+			self.third_phase =  info["third_phase"]
+			self.trigger = info["trigger"]
+			self.confidence = int(info["confidence"])
+			self.model_width = int(info["model_width"])
+			self.model_height = int(info["model_height"])
+			self.draw = int(info["draw"])
+			self.pr_coord = int(info["print_coord"])
+			self.orientation = info["orientation"]
+			
+			# security check params
+			self.security_check = int(info["security_check"])
+			self.sec_trigger = info["security_trigger"]
+			self.sec_confidence = int(info["security_confidence"])
+			self.sec_trig_xlimit = int(info["sec_trig_xlimit"])
+			self.sec_trig_ylimit = int(info["sec_trig_ylimit"])
+			input_path = info["input"]
+			output = info["output"]
+			fps= info["fps"]
+			return input_path,fps,output
 
 	def phase_check(self,ch):
 		if ch > self.first_phase[0] and ch < self.first_phase[1]:
@@ -144,12 +177,12 @@ class SecurityCheck():
 						self.person_dict.pop(i)
 
 
-
-		img = cv2.putText(img, "Entered: "+str(self.entry_count) , (30,150), cv2.FONT_HERSHEY_SIMPLEX , 1,  (0, 0, 255) , 2, cv2.LINE_AA)
-		img = cv2.putText(img, "Exited: :"+str(self.exit_count) , (30,70), cv2.FONT_HERSHEY_SIMPLEX , 1,  (0, 0, 255) , 2, cv2.LINE_AA)
-		if self.security_check == 1:
-			img = cv2.putText(img, "Checked: :"+str(self.checked_count) , (30,110), cv2.FONT_HERSHEY_SIMPLEX , 1,  (0, 0, 255) , 2, cv2.LINE_AA)
-			img = cv2.putText(img, "Not Checked: :"+str(len(self.not_checked)) , (30,30), cv2.FONT_HERSHEY_SIMPLEX , 1,  (0, 0, 255) , 2, cv2.LINE_AA)
+		if self.draw == 1:			
+			img = cv2.putText(img, "Entered: "+str(self.entry_count) , (30,150), cv2.FONT_HERSHEY_SIMPLEX , 1,  (0, 0, 255) , 2, cv2.LINE_AA)
+			img = cv2.putText(img, "Exited: :"+str(self.exit_count) , (30,70), cv2.FONT_HERSHEY_SIMPLEX , 1,  (0, 0, 255) , 2, cv2.LINE_AA)
+			if self.security_check == 1:
+				img = cv2.putText(img, "Checked: :"+str(self.checked_count) , (30,110), cv2.FONT_HERSHEY_SIMPLEX , 1,  (0, 0, 255) , 2, cv2.LINE_AA)
+				img = cv2.putText(img, "Not Checked: :"+str(len(self.not_checked)) , (30,30), cv2.FONT_HERSHEY_SIMPLEX , 1,  (0, 0, 255) , 2, cv2.LINE_AA)
 
 		return img
 
@@ -164,57 +197,29 @@ if __name__ == '__main__':
 		file = args.config
 
 		project = SecurityCheck()
-		with open(file) as f:
-			info = json.load(f)
-			#model parameters
-			self.weightPath = info['weightPath']
-			self.metaPath = info['metaPath']
-			self.configPath = info['configPath']
-
-			self.entry_point = info["entry_point"]
-			self.exit_point = info["exit_point"]
-			self.first_phase = info["first_phase"]
-			self.second_phase = info["second_phase"]
-			self.third_phase =  info["third_phase"]
-			self.trigger = info["trigger"]
-			self.confidence = int(info["confidence"])
-			self.model_width = int(info["model_width"])
-			self.model_height = int(info["model_height"])
-			self.draw = int(info["draw"])
-			self.pr_coord = int(info["print_coord"])
-			self.orientation = info["orientation"]
-			
-			# security check params
-			self.security_check = info["security_check"]
-			self.sec_trigger = info["security_trigger"]
-			self.sec_confidence = info["security_confidence"]
-			self.sec_trig_xlimit = info["sec_trig_xlimit"]
-			self.sec_trig_ylimit = info["sec_trig_ylimit"]
-
-			# input could be video file or stream
-			cap = cv2.VideoCapture(info["input"])
-			fps = info["fps"]
-			#network, class_names, class_colors = darknet.load_network(configPath,metaPath,weightPath,batch_size=1)
-			#darknet_image = darknet.make_image(darknet.network_width(network),darknet.network_height(network),3)
-			self.x_res = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-			self.y_res  = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT)) 
-			loc=info["output"]
-			out=cv2.VideoWriter(info["output"], fourcc,fps , (self.x_res,self.y_res), True)
-			while cap.isOpened(): 
-				ret,img=cap.read()
-				if ret is True:
-					frame_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-				else:
-					break
-				# load model
-				#frame_resized = cv2.resize(frame_rgb,(darknet.network_width(network),darknet.network_height(network)),interpolation=cv2.INTER_LINEAR)
-				#darknet.copy_image_from_bytes(darknet_image,frame_resized.tobytes())
-				#result=darknet.detect_image(network,class_names,darknet_image, thresh=0.25)
-				img = SecurityCheck.Security_entryGate(img,result)
-				out.write(img)
-			cap.release()
-			out.release()
-			f.close()
+		input_path,fps,output = project.load_file(file)
+		# input could be video file or stream
+		cap = cv2.VideoCapture(input_path)
+		#network, class_names, class_colors = darknet.load_network(configPath,metaPath,weightPath,batch_size=1)
+		#darknet_image = darknet.make_image(darknet.network_width(network),darknet.network_height(network),3)
+		project.x_res = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+		project.y_res  = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT)) 
+		out=cv2.VideoWriter(output, fourcc,fps , (project.x_res,project.y_res), True)
+		while cap.isOpened(): 
+			ret,img=cap.read()
+			if ret is True:
+				frame_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+			else:
+				break
+			# load model
+			#frame_resized = cv2.resize(frame_rgb,(darknet.network_width(network),darknet.network_height(network)),interpolation=cv2.INTER_LINEAR)
+			#darknet.copy_image_from_bytes(darknet_image,frame_resized.tobytes())
+			#result=darknet.detect_image(network,class_names,darknet_image, thresh=0.25)
+			img = project.Security_entryGate(img,result)
+			out.write(img)
+		cap.release()
+		out.release()
+		f.close()
 	except Exception as e:
 		print (str(e))
 	cv2.destroyAllWindows()
